791049884ecd771351242ca4cd638e7522665ffd
NJ3355 <njohn33555@gmail.com>
2016-08-05 13:52:19 -0400

    Combining 2 API's and Skycons
    -Added geocoding API to convert zipcode into long/latt so forecast.io
    can use coordinates to find weather data
    -Playing around with skyconns for the weather images

--- app.js
+++ app.js
@@ -1,25 +1,81 @@
+//Weather API
+
+      var icons = new Skycons(),
+          list  = [
+            "clear-day", "clear-night", "partly-cloudy-day",
+            "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+            "fog"
+          ],
+          i;
+      for(i = list.length; i--; )
+        icons.set(list[i], list[i]);
+      icons.play();
+
 var weather;
 
 var text = document.getElementById("zipcode");
 var button = document.getElementById("submit");
 button.addEventListener("click", gettingJSON);
 
-var api = 'http://api.openweathermap.org/data/2.5/weather?';
-var apiKey = '&appid=5ffa7f2c54d5a1333c53aa49a830fa30';
+var api = 'https://api.forecast.io/forecast/';
+var apiKey = '1e64d3848db562fa6695bffbc65fc1ef/';
 var zipcode; 
-var units = '&units=imperial'
-var url;
+var urlGeo, urlFore;
+var lon = 0;
+var lat = 0;
+
+//Geocoding API
+
+/*var geoAPI = 'http://maps.googleapis.com/maps/api/geocode/json?address=';
+var geoKey = 'AIzaSyBngpUQb0BvvotBHJ3eLG7ldtnXxlsapaY';
+var geoURL = geoAPI + geoKey;*/
+
 
 
 
    
 function gettingJSON(){
-    zipcode = 'zip=' + text.value + ',us';
-    url = api + zipcode + apiKey + units;
-    console.log(url);
+    zipcode = text.value;
+    
+    
 
-      $.getJSON(url, function(json){
+  urlGeo = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + zipcode;
 
+ $.ajax({
+     url: urlGeo,
+     dataType: "json",
+     success: function(data){
+      lat = data.results[0].geometry.bounds.northeast.lat;
+      lon = data.results[0].geometry.bounds.northeast.lng;
+
+
+           urlFore = api + apiKey + lat + ',' + lon;
+
+           $.ajax({
+                url: urlFore,
+                dataType: "jsonp",
+                success: function(data2){
+                  
+                var image = data2.currently.icon;
+                var temp = data2.currently.temperature;
+                var summary = data2.currently.summary;
+
+                  
+                }
+              });
+
+        
+      }
+    });
+
+
+
+
+  
+    
+
+     /* $.getJSON(url, function(json){
+console.log(json);
           weather = json;
           var image =  weather.weather[0].icon;
           var temp = weather.main.temp;
@@ -41,9 +97,9 @@ function gettingJSON(){
           }
           $('#info').append("<p>" + description + "</p>")
 
-          $('#credentials').hide();
+          $('#credentials').hide();*/
           
-       });
+       //});
 
  }
 
